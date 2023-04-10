d1bb5bbfc3cf52f9eb98565030ce3b56f1749179
Jamie <cajunlat@icloud.com>
2023-04-10 12:40:14 -0400

    -add GitHub repo -add folders MVVM -add ContentView as Environmental Object

--- LearningAppV1/LearningAppV1App.swift
+++ LearningAppV1/LearningAppV1App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LearningAppV1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView().environmentObject(ContentModel())
         }
     }
 }
---
+++ LearningAppV1/ViewModels/ContentModel.swift
@@ -0,0 +1,15 @@
+//
+//  ContentModel.swift
+//  LearningAppV1
+//
+//  Created by Jamie Latiolais on 4/10/23.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+    
+}
