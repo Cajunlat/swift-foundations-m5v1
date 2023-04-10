e6dbf5e10be80db02fec9625395214f97683153f
Jamie <cajunlat@icloud.com>
2023-04-10 12:31:45 -0400

    Initial Commit

---
+++ LearningAppV1/LearningAppV1App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppV1App.swift
+//  LearningAppV1
+//
+//  Created by Jamie Latiolais on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
